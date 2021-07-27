109da2b88483e2d56bc70760431db2f00d916148
AbdullahElian1 <abodelian28@gmail.com>
2021-07-27 18:48:04 +0300

    merge function

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -13,3 +13,12 @@ def parse_template(word:str)->str:
  actual_stripped=re.sub('{.*?}','{}',word)
  return actual_stripped,actual_part
 
+def merge(string_text:str,word_to_add:tuple):
+    final_text=string_text.format(*word_to_add)
+    with open('assets/result.txt','w') as output:
+        output.write(final_text)
+    return final_text
+
+
+
+
