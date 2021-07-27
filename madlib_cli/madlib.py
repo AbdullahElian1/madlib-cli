ada1a7803750fb06a10423e5609c73c06b60a94f
AbdullahElian1 <abodelian28@gmail.com>
2021-07-27 18:47:18 +0300

    parse function

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -8,3 +8,8 @@ def read_template(file_path:str)->str:
          print(file_path)
          raise FileNotFoundError
 
+def parse_template(word:str)->str:
+ actual_part=tuple(re.findall(r'{(.*?)}',word))
+ actual_stripped=re.sub('{.*?}','{}',word)
+ return actual_stripped,actual_part
+
