62924128d66a8ee986788deca07695e32e11476d
AbdullahElian1 <abodelian28@gmail.com>
2021-07-27 18:46:41 +0300

    read_templet function

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,11 +1,10 @@
+import re
+def read_template(file_path:str)->str:
+    try:
+     with open (file_path) as file:
+        content=file.read()
+        return content.strip()
+    except:
+         print(file_path)
+         raise FileNotFoundError
 
-def read_template():
-    pass
-
-
-def parse_template():
-    pass
-
-
-def merge ():
-    pass
