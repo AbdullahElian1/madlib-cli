2cead4ddcffe5d63ef2cd3442d59239b06333660
AbdullahELian1 <abodelian28@gmail.com>
2021-08-22 18:17:59 +0300

    test git hub action

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -10,9 +10,9 @@ def read_template(file_path:str)->str:
 
 def parse_template(word:str)->str:
  actual_part=tuple(re.findall(r'{(.*?)}',word))
- actual_stripped=re.sub('{.*/k?}','{}',word)
+ actual_stripped=re.sub('{.*?}','{}',word)
  return actual_stripped,actual_part
-#teeeest
+
 def merge(string_text:str,word_to_add:tuple):
     final_text=string_text.format(*word_to_add)
     with open('assets/result.txt','w') as output:
